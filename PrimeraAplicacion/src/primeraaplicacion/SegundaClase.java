5b3f2451f6c6899d66b8fab9f63ec5597d557d10
34605 <34605@LAPTOP-F25Q6MTU>
2020-11-02 14:28:29 +0100

    Escrito hola mundo

--- PrimeraAplicacion/src/primeraaplicacion/SegundaClase.java
+++ PrimeraAplicacion/src/primeraaplicacion/SegundaClase.java
@@ -9,13 +9,6 @@ package primeraaplicacion;
  *
  * @author 34605
  */
-public class PrimeraAplicacion {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        System.out.println("Hola Mundo");
-    }
+public class SegundaClase {
     
 }
